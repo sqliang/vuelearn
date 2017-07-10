14ca9a4e9233a3ca3bf597aab076602895e3ecae
liangshiquan <liangshiquan@baidu.com>
2017-07-10 11:10:16 +0800

    add the articlelists of admin

--- server/action/api/article/articleLists.js
+++ server/action/api/article/articleLists.js
@@ -14,7 +14,7 @@ module.exports = function (req, res, next) {
         {
             '_id': '5937b8173fa8952a4778a358',
             'contentToMark': '<p>决策树呀决策树2。。。</p>\n',
-            'classify': '机器学习',
+            'classify': '神经网络',
             'title': '决策树',
             'content': '决策树呀决策树。。。'
         },
@@ -22,7 +22,7 @@ module.exports = function (req, res, next) {
             '_id': '5937b8173fa8952a4778a398',
             'contentToMark': '<p>决策树呀决策树3。。。</p>\n',
             'classify': '机器学习',
-            'title': '决策树',
+            'title': '卷积神经网络',
             'content': '决策树呀决策树。。。'
         }
     ];
